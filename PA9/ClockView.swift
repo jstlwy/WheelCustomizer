b167a6425cc0e64df961208578a7f166910cf60c
jlbaixo <jlbsw20@gmail.com>
2016-04-20 11:18:17 -0700

    Update ClockView.swift

--- PA9/ClockView.swift
+++ PA9/ClockView.swift
@@ -10,28 +10,32 @@ import UIKit
 
 class ClockView: UIView {
     
+    // Fundamental elements
     var clockRadius: CGFloat!
     var clockCenterPoint: CGPoint!
     var clockPath: UIBezierPath!
     
-    var timeIntervals: [Int] = [
-        0,  // Element 0 = seconds
-        0,  // Element 1 = minutes
-        0   // Element 2 = hours
-    ]
+    // Clock timer
+    var clockTimer: NSTimer!
+    var clockTimerIsPaused: Bool!
+    
+    // Time intervals
+    var seconds: Int = 0
+    var minutes: Int = 0
+    var hours: Int = 0
     
     // Clock part measurements
     var clockBezelWidth: CGFloat!
     var minuteHandLength: CGFloat!
     var hourHandLength: CGFloat!
-    var handLengths = [CGFloat]()
     
     // Clock part colors
-    let clockRimColor = UIColor.blackColor()
+    let clockBezelColor = UIColor.blackColor()
     let clockFaceColor = UIColor.whiteColor()
-    let handColors: [UIColor] = [UIColor.redColor(), UIColor.greenColor(), UIColor.blueColor()]
+    let secondHandColor: UIColor = UIColor.redColor()
+    let minuteHandColor: UIColor = UIColor.greenColor()
+    let hourHandColor: UIColor = UIColor.blueColor()
 
-    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
 
@@ -40,7 +44,7 @@ class ClockView: UIView {
             y: (self.bounds.height / 2)
         )
         
-        clockBezelWidth = 2.5
+        clockBezelWidth = 3
         
         if self.bounds.height > self.bounds.width {
             clockRadius = CGFloat((self.bounds.width / 2) - clockBezelWidth)
@@ -48,79 +52,95 @@ class ClockView: UIView {
             clockRadius = CGFloat((self.bounds.height / 2) - clockBezelWidth)
         }
         
-        clockPath = UIBezierPath(
-            arcCenter: clockCenterPoint,
-            radius: clockRadius,
+        minuteHandLength = clockRadius * (2/3)
+        hourHandLength = clockRadius * (1/2)
+        
+        clockTimer = NSTimer.scheduledTimerWithTimeInterval(
+            1.0,
+            target: self,
+            selector: Selector("incrementByOneSecond:"),
+            userInfo: nil,
+            repeats: true
+        )
+        
+        clockTimerIsPaused = false
+    }
+    
+    func incrementByOneSecond(timer: NSTimer) {
+        seconds++
+        minutes = (seconds / 60)
+        hours = (minutes / 60)
+        
+        setNeedsDisplay()
+    }
+
+    override func drawRect(rect: CGRect) {
+        // Draw the outside of the clock
+        drawCircle(clockCenterPoint, radius: clockRadius, borderWidth: clockBezelWidth, borderColor: clockBezelColor, innerColor: clockFaceColor)
+        
+        // Draw second hand
+        drawClockHand(seconds, handLength: clockRadius, handColor: secondHandColor, amountPerCircle: 60)
+        // Draw minute hand
+        drawClockHand(minutes, handLength: minuteHandLength, handColor: minuteHandColor, amountPerCircle: 60)
+        // Draw hour hand
+        drawClockHand(hours, handLength: hourHandLength, handColor: hourHandColor, amountPerCircle: 12)
+        
+        // Draw center hub of clock
+        drawCircle(clockCenterPoint, radius: clockRadius / 10, borderWidth: 0, borderColor: clockBezelColor, innerColor: clockBezelColor)
+    }
+    
+    func drawCircle(centerPoint: CGPoint, radius: CGFloat, borderWidth: CGFloat, borderColor: UIColor, innerColor: UIColor) {
+        let circlePath = UIBezierPath(
+            arcCenter: centerPoint,
+            radius: radius,
             startAngle: 0,
             endAngle: CGFloat(2 * M_PI),
             clockwise: true
         )
         
-        clockPath.lineWidth = clockBezelWidth
+        circlePath.lineWidth = borderWidth
         
-        minuteHandLength = clockRadius * (2/3)
-        hourHandLength = clockRadius * (1/2)
+        let circleStrokeColor = borderColor
+        circleStrokeColor.setStroke()
+        circlePath.stroke()
         
-        handLengths.append(clockRadius)
-        handLengths.append(minuteHandLength)
-        handLengths.append(hourHandLength)
+        let circleFillColor = innerColor
+        circleFillColor.setFill()
+        circlePath.fill()
+
+        circlePath.closePath()
     }
     
+    func drawClockHand(timeInterval: Int, handLength: CGFloat, handColor: UIColor, amountPerCircle: Double) {
+        let handPath = UIBezierPath()
+        
+        handPath.moveToPoint(clockCenterPoint)
+        
+        handPath.addLineToPoint(CGPoint(
+            x: Double(clockCenterPoint.x) - Double(handLength) * cos(M_PI_2 + (Double(timeInterval) * 2 * M_PI / amountPerCircle)),
+            y: Double(clockCenterPoint.y) - Double(handLength) * sin(M_PI_2 + (Double(timeInterval) * 2 * M_PI / amountPerCircle))
+            ))
+
+        handPath.lineWidth = clockBezelWidth
+        handColor.setStroke()
+        
+        handPath.stroke()
+        handPath.closePath()
+    }
     
-    func incrementByOneSecond() {
-        if timeIntervals[0] + 1 == 60 {
-            timeIntervals[0] = 0
-            
-            if timeIntervals[1] + 1 == 60 {
-                timeIntervals[1] = 0
-                ++timeIntervals[2]
-            } else {
-                ++timeIntervals[1]
-            }
+    func hitTimerPauseButton() {
+        if clockTimerIsPaused == false {
+            clockTimer.invalidate()
         } else {
-            ++timeIntervals[0]
-        }
-        
-        setNeedsDisplay()
-    }
-
-    
-    override func drawRect(rect: CGRect) {
-        // Draw the outside of the clock
-        clockRimColor.setStroke()
-        clockPath.stroke()
-        
-        // Fill in the face color
-        clockFaceColor.setFill()
-        clockPath.fill()
-        
-        clockPath.closePath()
-        
-        // Draw the hands
-        for n in 0...2 {
-            let handLength: CGFloat = handLengths[n]
-            
-            let handPath = UIBezierPath(
-                arcCenter: clockCenterPoint,
-                radius: clockRadius,
-                startAngle: 0,
-                endAngle: CGFloat(2 * M_PI),
-                clockwise: true
+            clockTimer = NSTimer.scheduledTimerWithTimeInterval(
+                1.0,
+                target: self,
+                selector: Selector("incrementByOneSecond:"),
+                userInfo: nil,
+                repeats: true
             )
-
-            handPath.moveToPoint(clockCenterPoint)
-            handPath.lineWidth = clockBezelWidth
-            
-            handPath.addLineToPoint(CGPoint(
-                x: Double(clockCenterPoint.x) - Double(handLength) * cos(M_PI_2 + (Double(timeIntervals[n]) * 2 * M_PI / 60)),
-                y: Double(clockCenterPoint.y) - Double(handLength) * sin(M_PI_2 + (Double(timeIntervals[n]) * 2 * M_PI / 60))
-                ))
-            
-            let handColor = handColors[n]
-            handColor.setStroke()
-            handPath.stroke()
-            
-            handPath.closePath()
         }
+        
+        clockTimerIsPaused = !clockTimerIsPaused
     }
 }
